49bd5fe355bfc39cc2939f2d044a1d00d29dcaa9
Woolf <woolfson1995@gmail.com>
2025-06-30 14:23:46 +0300

    fix app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,8 +6,8 @@ import { RestrictedRoute } from "../RestrictedRoute";
 import Loader from "../Loader/Loader";
 
 const Layout = lazy(() => import("../Layout/Layout"));
-// const NotFound = lazy(() => import("../NotFound/NotFound"));
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
+
+const NotFoundPage = lazy(() => import ("../../pages/NotFoundPage/NotFoundPage"));
 
 const MainPage = lazy(() => import("../../pages/MainPage/MainPage"));
 const RecipeViewPage = lazy(() =>
@@ -54,7 +54,6 @@ export default function App() {
               <RestrictedRoute component={<AuthPage />} redirectTo="/" />
             }
           />
-          {/* <Route path="*" element={<NotFound />} /> */}
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
