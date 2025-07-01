dd87a89375f70c847af9a0292579e8395e86827a
prafessor <olegmilyo@gmail.com>
2025-07-01 23:09:20 +0300

    add component recipeDetails

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,5 +1,59 @@
+import clsx from "clsx";
 import css from "./RecipeDetails.module.css";
 
-export default function RecipeDetails() {
-  return <></>;
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+  
+  const instruction = recipe.instructions.split(/\n/g);
+
+  return <section className="section">
+    <div className={clsx('container', css.container)}>
+      <h1 className={css.title}>{recipe.title}</h1>
+      <img className={css.image} src={recipe.thumb} alt={recipe.description} />
+
+      <div className={css.wrapper}>
+        <div className={css.info_container}>
+          <div className={css.info_wrapper}>
+            <h2 className={css.subtitle}>General informations</h2>
+            <p className={css.text}><span className={css.info_accent_text}>Category:</span> {recipe.category}</p>
+            <p className={css.text}><span className={css.info_accent_text}>Cooking time:</span> {recipe.time} minutes</p>
+            <p className={css.text}><span className={css.info_accent_text}>Caloric content:</span> Approximately 200 kcal per serving</p>
+          </div>
+
+          <button className={css.btn} type="button">
+            Save
+            <svg width="24" height="24">
+              <use href="../src/assets/sprite.svg#icon-genericbookmark-alternative"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div className={css.main_text_wrapper}>
+          <div>
+            <h2 className={`${css.subtitle} ${css.about_title}`}>About recipe</h2>
+            <p className={css.text}>{recipe.description}</p>
+          </div>
+
+          <div>
+            <h2 className={`${css.subtitle} ${css.about_title}`}>Ingredients:</h2>
+            <ul className={css.ingredients_list}>
+              {recipe.ingredients.map(el => (
+                <li className={css.text} key={el.ingredient._id}>{el.ingredient.name} — {el.measure}</li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className={`${css.subtitle} ${css.instruction_title}`}>Preparation Steps:</h2>
+            <ol className={css.instruction_list}>
+              {instruction.map((el, idx) => (
+                <li className={css.text} key={idx}>{el}</li>
+              ))}
+            </ol>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>;
 }
--- src/pages/RecipeViewPage/RecipeViewPage.jsx
+++ src/pages/RecipeViewPage/RecipeViewPage.jsx
@@ -1,12 +1,33 @@
+import { useParams } from 'react-router-dom';
 import NotFound from '../../components/NotFound/NotFound';
 import RecipeDetails from '../../components/RecipeDetails/RecipeDetails';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function RegistrationPage() {
+export default function RecipeViewPage() {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState(null);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getRecipe = async () => {
+      try {
+        setIsError(false)
+        const recipeResponse = await axios.get(`https://byte-bitebd.onrender.com/api/recipes/${id}`);
+
+        setRecipe(recipeResponse.data.data);
+      } catch {
+        setIsError(true);
+      }
+    }
+
+    getRecipe();
+  }, [id]);
 
   return (
     <>
-      <NotFound />
-      <RecipeDetails />
+      {isError && <NotFound />}
+      {recipe && <RecipeDetails recipe={recipe} />}
     </>
   );
 }
