28400959506cac3b0e4d59eda0eb80d2a7a55b7a
Woolf <woolfson1995@gmail.com>
2025-07-02 10:29:03 +0300

    add new root

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,28 +1,28 @@
-import { FaUser } from "react-icons/fa6";
-import { FaPhone } from "react-icons/fa";
+// import { FaUser } from "react-icons/fa6";
+// import { FaPhone } from "react-icons/fa";
 import css from "./RecipeCard.module.css";
-import { useDispatch } from "react-redux";
-import { setDeleteRecipeId, setEditRecipeId } from "../../redux/recipes/slice";
+// import { useDispatch } from "react-redux";
+// import { setDeleteRecipeId, setEditRecipeId } from "../../redux/recipes/slice";
 
 function RecipeCard({ data }) {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const handleDeleteButton = () => {
-    dispatch(setDeleteRecipeId(data.id));
-  };
+  // const handleDeleteButton = () => {
+  //   dispatch(setDeleteRecipeId(data.id));
+  // };
 
-  const handleEditButton = () => {
-    dispatch(setEditRecipeId(data.id));
-  };
+  // const handleEditButton = () => {
+  //   dispatch(setEditRecipeId(data.id));
+  // };
 
-  console.log(data);
+  // console.log(data);
 
   return (
     <div className={css.recipe}>
       <div className={css.info}>{/* <FaUser /> {data.name} */}</div>
       <div className={css.info}>{/* <FaPhone /> {data.number} */}</div>
-      <button onClick={handleDeleteButton}>Delete</button>{" "}
-      <button onClick={handleEditButton}>Edit</button>
+      {/* <button onClick={handleDeleteButton}>Delete</button>{" "}
+      <button onClick={handleEditButton}>Edit</button> */}
     </div>
   );
 }
